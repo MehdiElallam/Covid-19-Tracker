ce012501efb4e922c7ce61e21f0c57d3acf892ae
Mehdi El <mehdielallam07@gmail.com>
2021-12-15 22:59:45 +0100

    Updating app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import './App.css';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import { Container, Row, Col } from 'react-bootstrap'
+import { BrowserRouter as Router } from 'react-router-dom'
+import RoutesList from './components/RoutesList';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Header title="Covid-19 Tracker" />
-    </div>
+    <Router>
+        <Header title="Covid-19 Tracker" />
+          <Container style={{
+            maxWidth:'98%'
+          }}>
+              <Row>
+                  <Col xs={12} md={12}>
+                    
+                  </Col>
+              </Row>
+              <Row>
+                  <Col xs={4} md={3}>
+                    <Sidebar />
+                  </Col>
+                  <Col xs={8} md={9}>
+                    <RoutesList />
+                  </Col>
+              </Row>
+          </Container>
+          
+    </Router>
   );
 }
 
---
+++ src/utils/config.js
@@ -0,0 +1,2 @@
+
+export const url = "https://disease.sh/v3/covid-19";
