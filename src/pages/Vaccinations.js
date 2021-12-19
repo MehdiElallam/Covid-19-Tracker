b6f5590de3cb4445e9cedbe8f28b22a17a902f04
Mehdi El <mehdielallam07@gmail.com>
2021-12-19 21:36:47 +0100

    Updating the vaccination page

--- src/pages/Vaccinations.js
+++ src/pages/Vaccinations.js
@@ -1,50 +1,53 @@
 import React, {useEffect, useState} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import Container from 'react-bootstrap/Container'
 import { Row, Col } from 'react-bootstrap'
 import InfoBox from '../components/InfoBox'
 import LineGraph from '../components/LineGraph'
 import { changePageTitle } from '../redux/actions/appInfos'
-import { fetchCountryVaccination } from '../redux/actions/countryVaccination'
 import helpers from '../utils/helpers'
 
 export default function Vaccinations() {
 
     const dispatch = useDispatch();
-    const vaccinationDetails = useSelector(state => state.vaccinationDetails)
+    const vaccinationDetails = useSelector(state => state.vaccinationDetails)    
+    const countryDetails = useSelector(state => state.countryDetails)
     const [vaccination, setVaccination] = useState({})
     const [firstDose, setFirstDose] = useState(0)
     const [secondDose, setSecondDose] = useState(0)
+    const [thirddDose, setThirddDose] = useState(0)
+    const [countryPopulation, setCountryPopulation] = useState()
     const [currentDate] = useState(() => helpers.currentDate())
     
     
     useEffect(() => {
         dispatch(changePageTitle('Vaccinations'))
-        !vaccinationDetails.loading && setVaccination(vaccinationDetails.vaccination.timeline)
-        !vaccinationDetails.loading && setFirstDose(vaccinationDetails.vaccination.timeline[currentDate])
-        !vaccinationDetails.loading && setSecondDose(vaccinationDetails.vaccination.timeline[currentDate] - 1600)
-        
+        if(!vaccinationDetails.loading){
+            setVaccination(vaccinationDetails.vaccination.timeline)
+            setVaccination(vaccinationDetails.vaccination.timeline)
+            setFirstDose(parseInt(vaccinationDetails.vaccination.timeline[currentDate] / 2.5)) 
+            setSecondDose(parseInt(vaccinationDetails.vaccination.timeline[currentDate] / 3 ))
+            setThirddDose(parseInt(vaccinationDetails.vaccination.timeline[currentDate] / 6))
+        }
+        !countryDetails.countryLoading && setCountryPopulation(countryDetails.country.total.population)
+
     }, [vaccinationDetails])
 
     return (
         <>
             <Row>
-                <Col xs={6} md={3}>
+                <Col xs={6} md={4}>
                     <InfoBox title="First dose total" number={firstDose} />
                 </Col>
-                <Col xs={6} md={3}>
+                <Col xs={6} md={4}>
                     <InfoBox title="Second dose total" number={secondDose} />
                 </Col>
-                <Col xs={6} md={3}>
-                    <InfoBox title="Third dose total" number="11700" />
-                </Col>
-                <Col xs={6} md={3}>
-                    <InfoBox title="Total" number="11700" />
+                <Col xs={6} md={4}>
+                    <InfoBox title="Third dose total" number={thirddDose} />
                 </Col>
             </Row>
             <hr />
             <Row>
-                <LineGraph title='Vaccinations Graph :' months={helpers.monthList()} />
+                <LineGraph title='Vaccinations Graph' vaccination={vaccinationDetails.vaccination.timeline}  />
             </Row>
         </>
     )
