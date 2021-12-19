8894206dccb12f641340ef13810e91983f1e8d61
Mehdi El <mehdielallam07@gmail.com>
2021-12-19 21:36:20 +0100

    Adding a sass file to the App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React, { useEffect, useState} from 'react'
+import './App.scss'
 import { useSelector, useDispatch } from 'react-redux'
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import { Container, Row, Col } from 'react-bootstrap'
 import { BrowserRouter as Router } from 'react-router-dom'
-import RoutesList from './components/RoutesList';
+import RoutesList from './routes';
 import CountrySelector from './components/CountrySelector'
 import { fetchCountryHistorical } from "./redux/actions/countryDetails"
-import LineGraph from './components/LineGraph';
-
-
 
 
 function App() {
 
   const dispatch = useDispatch();
-
   useEffect(() => {
-      dispatch(fetchCountryHistorical('UK'));
+      dispatch(fetchCountryHistorical());
   }, [])
 
 
@@ -27,11 +24,7 @@ function App() {
           <Container style={{
             maxWidth:'98%'
           }}>
-              <Row>
-                  <Col xs={12} md={12}>
-                    
-                  </Col>
-              </Row>
+              
               <Row>
                   <Col xs={12} md={3}>
                     <Sidebar />
