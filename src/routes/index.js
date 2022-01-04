8b840e85a54a8b19853558c78603c00f8c526500
Mehdi El <mehdielallam07@gmail.com>
2022-01-04 12:05:57 +0100

    Adding React lazy to the routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Routes , Route } from 'react-router-dom'
-import Home from '../pages/Home'
-import Cases from '../pages/Cases'
-import Vaccinations from '../pages/Vaccinations'
-import Deaths from '../pages/Deaths'
 import PageNotFound from '../pages/PageNotFound'
+
+const Home = lazy(() => import('../pages/Home'));
+const Cases = lazy(() => import('../pages/Cases'));
+const Vaccinations = lazy(() => import('../pages/Vaccinations'));
+const Deaths = lazy(() => import('../pages/Deaths'));
 
 export default function RoutesList() {
     
     return (
-        <Routes>
-            <Route exact={true} path="/" element={<Home />} />
-            <Route path="/cases" element={<Cases />} />
-            <Route path="/vaccinations" element={<Vaccinations/>} />
-            <Route path="/deaths" element={<Deaths/>} />
-            <Route path='*' exact={true} element={<PageNotFound />} />
-        </Routes>
+        <Suspense fallback={<div>Loading Data  by another server... </div>} >
+            <Routes>
+                <Route exact={true} path="/" element={<Home />} />
+                <Route path="/cases" element={<Cases />} />
+                <Route path="/vaccinations" element={<Vaccinations/>} />
+                <Route path="/deaths" element={<Deaths/>} />
+                <Route path='*' exact={true} element={<PageNotFound />} />
+            </Routes>
+        </Suspense>
     )
 }
